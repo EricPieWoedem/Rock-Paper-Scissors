7e735ca91be4840545b9cff12b05ea895da3d96f
EricPieWoedem <coco.pie3030@gmail.com>
2024-07-13 20:45:18 +0000

    fix bug in third function

--- javascript.js
+++ javascript.js
@@ -16,11 +16,53 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let userInput = prompt("Enter your choice");
-    if (userInput == "rock" || userInput == "paper" || userInput == "scissors{"){
+    if (userInput == "rock" || userInput == "paper" || userInput == "scissors"){
         return(userInput);
     }
     else {
         return ("wrong input");
     }
 } 
+
+let humanScore = 0;
+let computerScore = 0;
+
+let win = "You win!";
+let draw = "It's a draw!";
+let lose = "You lose";
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice,computerChoice) {
+    for ( let i = 0; i <= 5, i++;) {
+        if (humanSelection == "paper" && computerSelection == "rock") {
+            return(win);
+            humanScore++;
+        }
+        else if(humanSelection == "rock" && computerSelection == "scissors") {
+            return(win);
+            humanScore++;
+
+        }
+        else if(humanSelection == "scissors" && computerSelection == "paper" ) {
+            return(win);
+            humanScore++;
+        }
+
+        else if(humanChoice == computerChoice) {
+            return(draw);
+        }
+
+        else {
+            return(lose);
+            computerScore++;
+        }
+    }
+}
+
+
+playRound(humanSelection,computerSelection);
+
+
 
